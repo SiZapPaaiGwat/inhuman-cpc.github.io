a766b7dca0741a85b106ebcf634e82616e192c0b
simongfxu <xugaofan@gmail.com>
2016-08-15 12:04:32 +0800

    Update 网页正文提取

--- md-extraction/DOMParser.js
+++
@@ -1,8 +0,0 @@
-var htmlparser = require('htmlparser2')
-
-module.exports = function parse(html) {
-  var handler = new htmlparser.DomHandler()
-  var parser = new htmlparser.Parser(handler)
-  parser.parseComplete(html)
-  return handler.dom
-}
